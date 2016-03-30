a1690cf70aa47b1bd14483761813586800eff964
stpulsipher <steve.pulse5@gmail.com>
2016-03-30 16:52:45 +0000

    search works. changed search to get. added search syntax for information about queries

--- routes/index.js
+++ routes/index.js
@@ -58,11 +58,75 @@ router.put('/item:ferretList', function(req, res, next) {
 	});
 });
 
-router.post('/search', function(req, res, next) {
-	console.log("IN SEARCH");
-	var myRe = new RegExp("^" + req.query.q);
-	console.log(myRe);
-	res.sendStatus(200);
+router.get('/search', function(req, res, next) {
+	var description = req.query.q;
+	var condition = req.query.condition;
+	var category = req.query.category;
+	var location = req.query.location;
+	var price = req.query.price;
+	
+	console.log("description: " + description);
+	console.log("condition: " + condition);
+	console.log("category: " + category);
+	console.log("location: " + location);
+	console.log("price: " + price);
+	
+	var que = new RegExp(description);
+	var con = new RegExp(condition);
+	var cat = new RegExp(category);
+	var loc = new RegExp(location);
+
+	ferretList.find(function(err,items) {
+		if (err) return console.error(err);
+		else {
+			var jsonresult = [];
+			for(var i = 0; i < items.length; i++) {
+				var result1 = items[i].description.search(que);
+				var result2 = items[i].item.search(que);
+				var result3 = items[i].condition.search(con);
+				var result4 = items[i].category.search(cat);
+				var result5 = items[i].location.search(loc);
+				var result6 = -1;
+				if(items[i].price <= price){
+					result6 = 1;
+				}
+				
+				var found = 1;
+				if(description != null){
+					if(result1 == -1 && result2 == -1){
+						found = 0;
+					}
+				}
+				if(condition != null){
+					if(result3 == -1){
+						found = 0;
+					}
+				}
+				if(category != null){
+					if(result4 == -1){
+						found = 0;
+					}
+				}
+				if(location != null){
+					if(result5 == -1){
+						found = 0;
+					}
+				}
+				if(price != null){
+					if(result6 == -1){
+						found = 0;
+					}
+				}
+				
+				
+				if(found == 1) {
+					jsonresult.push({item:items[i]});
+				}
+			}
+			console.log(jsonresult);
+			res.status(200).json(jsonresult);
+		}
+	})
 });
 
 module.exports = router;
