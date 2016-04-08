163f362a2f7c6f764061e5be7be627df8ca93ba5
Daniel Lee <leechef2010@gmail.com>
2016-04-08 03:00:16 +0000

    Added Add Item modal

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,5 @@
-angular.module('ferretList', [])
-.controller('MainCtrl', [
+angular.module('ferretList', ['ngAnimate', 'ui.bootstrap']);
+angular.module('ferretList').controller('MainCtrl', [
 	'$scope', '$http',
 	function($scope,$http){
 		$scope.test = "Hello World!";
@@ -13,3 +13,55 @@ angular.module('ferretList', [])
 		$scope.getItems();
 	}
 ]);
+
+angular.module('ferretList').controller('ModalCtrl', function ($scope, $uibModal, $log) {
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.animationsEnabled = true;
+
+  $scope.open = function (size) {
+
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+
+});
+
+// Please note that $uibModalInstance represents a modal window (instance) dependency.
+// It is not the same as the $uibModal service used above.
+
+angular.module('ferretList').controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $uibModalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
